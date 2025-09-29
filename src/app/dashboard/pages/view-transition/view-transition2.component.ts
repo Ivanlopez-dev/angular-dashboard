ee38c67c819ab0ca3de3b058ece8186770a44015
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-29 12:33:00 +0200

    add: ✨ view transitions API

---
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  selector: 'app-view-transition',
+  imports: [TitleComponent],
+  template: `
+    <app-title title="View Transition 2" />
+
+    <section class="flex justify-start">
+      <img
+        srcset="https://picsum.photos/id/237/200/300"
+        alt="Picsum example image"
+        width="200"
+        height="300"
+      />
+
+      <div class="bg-blue-500 w-56 h-56">
+
+      </div>
+    </section>
+  `,
+
+})
+export default class ViewTransitionComponent { }
