9b32f8d644ba28fd1edcd5360910982d2f87fbba
Ivan López Rodríguez <ivaan100@gmail.com>
2025-09-29 12:40:35 +0200

    add: ✨ view transitions - hero animation

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -14,9 +14,13 @@ import { TitleComponent } from '@shared/title/title.component';
         alt="Picsum example image"
         width="200"
         height="300"
+        style="view-transition-name: hero1"
       />
 
-      <div class="bg-blue-500 w-56 h-56">
+      <div
+        class="bg-blue-500 w-56 h-56"
+        style="view-transition-name: hero2"
+      >
 
       </div>
     </section>
--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -8,15 +8,19 @@ import { TitleComponent } from '@shared/title/title.component';
   template: `
     <app-title title="View Transition 2" />
 
-    <section class="flex justify-start">
+    <section class="flex justify-end">
       <img
         srcset="https://picsum.photos/id/237/200/300"
         alt="Picsum example image"
         width="200"
         height="300"
+        style="view-transition-name: hero1"
       />
 
-      <div class="bg-blue-500 w-56 h-56">
+      <div
+        class="fixed right-56 bg-purple-500 w-56 h-56 rounded-2xl"
+        style="view-transition-name: hero2"
+      >
 
       </div>
     </section>
